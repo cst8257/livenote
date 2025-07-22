16e8774d8f671638a96882ff981d4324df0c4fd2
Michael Eisenbraun <eisenbm@algonquincollege.com>
2025-07-22 00:02:58 -0400

    Creates starter files for Part 3

---
+++ resources/views/livewire/login.blade.php
@@ -0,0 +1,20 @@
+<main class="container py-5">
+    <header class="text-center p-5">
+        <h1 class="display-4">Login</h1>
+    </header>
+    <div class="row">
+        <div class="col col-md-6 offset-md-3 col-xl-4 offset-xl-4">
+            <form class="bg-light p-5 border border-1 mb-3">
+                <div class="form-group mb-3">
+                    <label class="form-label">Email</label>
+                    <input type="email" class="form-control" wire:model="email">
+                </div>
+                <div class="form-group mb-3">
+                    <label class="form-label">Password</label>
+                    <input type="password" class="form-control" wire:model="password">
+                </div>
+                <button class="btn btn-primary">Login</button>
+            </form>
+        </div>
+    </div>
+</main>
